45a9018730376327e26a89e7c19f71c43bbe8f61
Edwin Martinez <martinez92edwin@gmail.com>
2019-08-05 16:59:57 -0700

    Edited kaman_filter.cpp

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -26,16 +26,65 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+  x_ = F_ * x_;
+
+  MatrixXd F_transpose = F_.F_transpose();
+  P_ = F_ * P_ * F_transpose +Q_;
+
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+  MatrixXd Ht = H.transpose();
+  MatrixXd Si = S.inverse();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+
+  VectorXd y = z - H_ * x_;
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd K =  P_ * Ht * Si;
+
+  // new state
+  x_ = x_ + (K * y);
+  P_ = (I - K * H_) * P_;
+
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+  float px=x_(0);
+  float py=x_(1);
+  float vx=x_(2);
+  float vy=x_(3);
+
+  float rho = sqrt(px*px + py*py);
+  float phi = atan2(py, px);
+  float rho_dot = (px*vx + py*vy)/rho;
+
+  VectorXd h = VectorXd(3);
+  h << rho, phi, rho_dot;
+
+  VectorXd y = z - h;
+
+  if (y(1) > PI){
+    y(1) = y(1) - 2*PI;
+  }
+  else if (y(1) < -PI){
+    y(1) = y(1) +2*PI;
+  }
+
+  MatrixXd Ht = H.transpose();
+  MatrixXd Si = S.inverse();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+
+  VectorXd y = z - H_ * x_;
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd K =  P_ * Ht * Si;
+
+  // new state
+  x_ = x_ + (K * y);
+  P_ = (I - K * H_) * P_;
 }
